dd6d708a4212a4972c0a107175bcb30d359f09df
rohitranjansinha <sinharohit511@gmail.com>
2020-08-12 01:07:33 +0530

    Initial commit

---
+++ app/src/main/java/com/rohit/project/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.rohit.project.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        this.getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LogIN.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
---
+++ app/src/main/java/com/rohit/project/myapplication/Profile.java
@@ -0,0 +1,47 @@
+ package com.rohit.project.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+ public class Profile extends AppCompatActivity {
+
+    TextView name;
+    TextView mail;
+    Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logout = findViewById(R.id.logout);
+        name = findViewById(R.id.name);
+        mail = findViewById(R.id.mail);
+
+        GoogleSignInAccount googleSignInAccount  = GoogleSignIn.getLastSignedInAccount(this);
+        if(googleSignInAccount != null){
+            name.setText(googleSignInAccount.getDisplayName());
+            mail.setText(googleSignInAccount.getEmail());
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(Profile.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
